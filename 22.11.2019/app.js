ec843c7e9ceef0f30c12adab6b5705c68685268c
dejo <d.dejanovic@dvcsolutions.com>
2019-11-22 08:52:33 +0100

    new tasks

---
+++ 22.11.2019/app.js
@@ -0,0 +1,23 @@
+/**
+ * Napisi funkciju koja za argument prima mjesec i godinu i treba da vrati sve dane za dati mjesec i godinu
+ * @param {number} month 
+ * @param {number} year
+ * @returns {number[]} days
+ */
+function solution(month, year) {
+    return days;
+}
+
+solution(9, 2019)
+
+
+/**
+ * Napisi funkciju koja za argument prima recenicu a vraca najduzu rijec unutar nje
+ * @param {string} str 
+ * @returns {string} reversed
+ */
+function reverseString(str) {
+    return reversed;
+}
+
+reverseString('I love to code in javaScript!')
