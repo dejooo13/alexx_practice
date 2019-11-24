d873447ca26218fd10de2a6083aacde991ac1d5f
Aleksandra Adamovic <acamojmail@gmail.com>
2019-11-24 17:44:36 +0100

    html, css and js practice

--- 22.11.2019/app.js
+++ 22.11.2019/app.js
@@ -5,10 +5,17 @@
  * @returns {number[]} days
  */
 function solution(month, year) {
+    let d =new Date(year, month, 0);
+    let days_in_month = d.getDate();
+    let days = [];
+    for(let i=1; i <= days_in_month; i++){
+        days.push(i);
+    }
+    console.log(days);
     return days;
 }
 
-solution(9, 2019)
+solution(9, 2019);
 
 
 /**
@@ -17,7 +24,20 @@ solution(9, 2019)
  * @returns {string} reversed
  */
 function reverseString(str) {
+    words = str.split(" ");
+    var i = 0;
+    var reversed = "";
+    while(i < words.length) {
+        letters = words[i].split('');
+        if(reversed == '') {
+            reversed = words[i];
+        }else if(reversed.split('').length <= words[i].split('').length){
+            reversed = words[i];
+        }
+        i++;
+    }
+    console.log(reversed);
     return reversed;
 }
 
-reverseString('I love to code in javaScript!')
+reverseString('I love to code in javaScript !')
