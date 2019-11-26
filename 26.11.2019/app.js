38201f6b946844bebeea98b4fe108f74480a4e89
dejo <d.dejanovic@dvcsolutions.com>
2019-11-26 19:30:01 +0100

    new tasks

---
+++ 26.11.2019/app.js
@@ -0,0 +1,24 @@
+/**
+ * Napravi "duplify" prototype na String objektu koji treba da datu rijec duplira onoliku puta koliko mu se proslijedi kao argument
+ * npr. "hello".duplify(5) -> hellohellohellohellohello
+ * @param {number} times How many times to duplicate string
+ */
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ /**
+  * Napraviti "Wallet" konstruktor funkciju koja za argument prima inicijalno stanje racuna
+  * @param {number} ammount How much money you have in your wallet
+  */
+
+
+ /**
+  * Napraviti prototip na Walletu koji se zove "deposit" koji za argument prima iznos koji se dodaje na ukupno stanje racuna (uvecava se njegovo stanje)
+  * @param {number} depo How much you want to add to your wallet
+  */
+
+
+ /**
+  * Napraviti prototip na Walletu koji se zove "withdraw" koji za argument prima iznos koji se oduzima od ukupnog stanja racuna (umanjuje se njegovo stanje)
+  * @param {number} wdr How much money you want to take from wallet
+  */
