e2959e82bb67f94e556d362bc0049071d3e9f297
Aleksandra Adamovic <acamojmail@gmail.com>
2019-11-27 23:34:34 +0100

    zadaci

--- 26.11.2019/app.js
+++ 26.11.2019/app.js
@@ -1,8 +1,19 @@
 /**
- * Napravi "duplify" prototype na String objektu koji treba da datu rijec duplira onoliku puta koliko mu se proslijedi kao argument
- * npr. "hello".duplify(5) -> hellohellohellohellohello
+ * Napravi "duplify" prototype na String objektu koji treba da datu rijec duplira onoliku puta koliko mu
+ *  se proslijedi kao argument npr. "hello".duplify(5) -> hellohellohellohellohello
  * @param {number} times How many times to duplicate string
  */
+
+String.prototype.duplify = function (times) {
+    let w = "";
+    while(times > 0){
+        w += this;
+        times--;
+    }
+    console.log(w);
+};
+
+'hello'.duplify(5);
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -11,14 +22,30 @@
   * @param {number} ammount How much money you have in your wallet
   */
 
+  function Wallet(ammount) {
+      this.ammount = ammount;
+  }
 
  /**
-  * Napraviti prototip na Walletu koji se zove "deposit" koji za argument prima iznos koji se dodaje na ukupno stanje racuna (uvecava se njegovo stanje)
-  * @param {number} depo How much you want to add to your wallet
+  * Napraviti prototip na Walletu koji se zove "deposit" koji za argument prima iznos koji se dodaje na ukupno stanje racuna 
+  * (uvecava se njegovo stanje) @param {number} depo How much you want to add to your wallet
   */
-
+  Wallet.prototype.deposit = function(depo){
+        this.ammount += depo
+        console.log(this.ammount);
+  }
 
  /**
-  * Napraviti prototip na Walletu koji se zove "withdraw" koji za argument prima iznos koji se oduzima od ukupnog stanja racuna (umanjuje se njegovo stanje)
-  * @param {number} wdr How much money you want to take from wallet
+  * Napraviti prototip na Walletu koji se zove "withdraw" koji za argument prima iznos koji se oduzima od ukupnog stanja racuna 
+  * (umanjuje se njegovo stanje) @param {number} wdr How much money you want to take from wallet
   */
+
+  Wallet.prototype.withdraw = function(wdr){
+        this.ammount -= wdr;
+        console.log(this.ammount);
+  }
+
+  var stanje = new Wallet(500);
+  stanje.deposit(60);
+  stanje.withdraw(30);
+
